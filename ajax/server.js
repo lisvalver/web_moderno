f4eaeda53c176315da985f5112c05f2c43367642
lisvalver <lisvalver.atte@gmail.com>
2020-06-03 17:34:32 -0300

    axios2

--- ajax/server.js
+++ ajax/server.js
@@ -36,5 +36,12 @@ app.post('/formulario', (req, res) => {
     })
 })
 
+app.get('/parOuImpar', (req, res) => {
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 
 app.listen(8080, () => console.log('Executando'))
